af2a1cdedf760558060204c84755e1abe4be188c
Madelyn Raymond <n01440101@unf.edu>
2022-08-29 02:38:21 -0400

    changed color scheme

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,4 +1,4 @@
-import { faMartiniGlass } from '@fortawesome/free-solid-svg-icons'
+import { faMartiniGlass, faCartShopping, faUserTie } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { useState } from 'react'
 import { BrowserRouter, Link, NavLink, Route, Routes } from 'react-router-dom'
@@ -13,11 +13,11 @@ function App() {
       
       <BrowserRouter>
       <div className="nav">
-          <NavLink to="/"><p className="title">Neon <FontAwesomeIcon icon={faMartiniGlass} /></p></NavLink>
+          <NavLink to="/"><p className="title">NEON ネオン</p></NavLink>
           <div className="navRight">
-              <NavLink to="/Menu"><p>Menu</p></NavLink>
-              <NavLink to="/Cart"><p>Cart</p></NavLink>
-              <NavLink to="/OrderQueue"><p>Staff</p></NavLink>
+              <NavLink to="/Menu"><p>Menu  <FontAwesomeIcon icon={faMartiniGlass} /></p></NavLink>
+              <NavLink to="/Cart"><p>Cart <FontAwesomeIcon icon={faCartShopping} /></p></NavLink>
+              <NavLink to="/OrderQueue"><p>Staff  <FontAwesomeIcon icon={faUserTie} /></p></NavLink>
           </div>
       </div>
         <Routes>
