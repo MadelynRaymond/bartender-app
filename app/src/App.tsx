c5092b4e6ba54ddb21eb3df50cb74d5dc7bca5d4
Madelyn Raymond <n01440101@unf.edu>
2022-08-29 01:58:58 -0400

    router added

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,4 +1,7 @@
+import { faMartiniGlass } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { useState } from 'react'
+import { BrowserRouter, Link, NavLink, Route, Routes } from 'react-router-dom'
 import './App.css'
 import CocktailMenu from './views/CocktailMenu'
 import LandingPage from './views/LandingPage'
@@ -7,7 +10,24 @@ function App() {
 
   return (
     <>
-      <LandingPage />
+      
+      <BrowserRouter>
+      <div className="nav">
+          <NavLink to="/"><p className="title">Neon <FontAwesomeIcon icon={faMartiniGlass} /></p></NavLink>
+          <div className="navRight">
+              <NavLink to="/Menu"><p>Menu</p></NavLink>
+              <NavLink to="/Cart"><p>Cart</p></NavLink>
+              <NavLink to="/OrderQueue"><p>Staff</p></NavLink>
+          </div>
+      </div>
+        <Routes>
+          <Route path="/" element={<LandingPage/>}></Route>
+          <Route path="/Menu" element={<CocktailMenu/>}></Route>
+          <Route path="/Cart" element={<CocktailMenu/>}></Route>
+          <Route path="/StaffLogin" element={<CocktailMenu/>}></Route>
+          <Route path="/OrderQueue" element={<CocktailMenu/>}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
--- app/src/views/LandingPage.tsx
+++ app/src/views/LandingPage.tsx
@@ -1,19 +1,8 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faMartiniGlass } from "@fortawesome/free-solid-svg-icons";
-
 import React from 'react'
 
 export default function LandingPage() {
   return (
     <>
-    <div className="nav">
-        <p className="title">Neon <FontAwesomeIcon icon={faMartiniGlass} /></p>
-        <div className="navRight">
-            <p>Menu</p>
-            <p>Cart</p>
-            <p>Staff Portal</p>
-        </div>
-    </div>
         <div className="landing">
             <div className="landingImage" style={{backgroundImage: 'url("./landing.webp")'}}>
                 <section className='landingImageContent'>
